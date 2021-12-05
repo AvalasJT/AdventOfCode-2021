2fd7a21771dff2433c35bdc8818c38f6935bf51d
AvalasJT <79806487+AvalasJT@users.noreply.github.com>
2021-12-05 15:19:45 +0100

    Update Day_05.py

--- Day_05.py
+++ Day_05.py
@@ -8,7 +8,7 @@ ground = np.zeros((1,1,2), int)
 for line in f:
     x1, y1, x2, y2 = [int(n) for n in line.replace(" -> ",",").split(",")]
     if max(x1, x2, y1, y2) > np.shape(ground)[0] - 1:
-        ground = np.append(ground, np.zeros((max(x1, x2, y1, y2)+1 - np.shape(ground)[0],np.shape(ground)[0],2)), 0) #less slow
+        ground = np.append(ground, np.zeros((max(x1, x2, y1, y2)+1 - np.shape(ground)[0],np.shape(ground)[1],2)), 0) #less slow
         ground = np.append(ground, np.zeros((np.shape(ground)[0],max(x1, x2, y1, y2)+1 - np.shape(ground)[1],2)), 1)
     
     if x1 == x2:
@@ -18,8 +18,7 @@ for line in f:
 
 #Part 2
     diff = max(abs(x2 - x1) ,abs(y2 - y1))
-    dx = int((x2 - x1) / diff) 
-    dy = int((y2 - y1) / diff)
+    dx, dy = int((x2 - x1) / diff), int((y2 - y1) / diff)
     
     for j in range(diff+1):
         ground[x1+j*dx, y1+j*dy, 1] += 1
