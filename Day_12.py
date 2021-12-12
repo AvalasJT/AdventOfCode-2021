500d00fd810dfd75f770d2a8474f2b585a87425d
AvalasJT <79806487+AvalasJT@users.noreply.github.com>
2021-12-12 18:41:22 +0100

    Update Day_12.py

--- Day_12.py
+++ Day_12.py
@@ -1,34 +1,31 @@
-f = open('input.txt', 'r')
-connections = [x.strip('\n').split('-') for x in f.readlines()]
-f.close()
-
-caves = set([cave for connection in connections for cave in connection])
-smallcaves = set([cave for cave in caves if all(c in 'abcdefghijklmnopqrstuvwxyz' for c in cave)])
+f = open('input_test.txt', 'r')
+connections = [x.strip().split('-') for x in f.readlines()]
 connections = connections + [[x[1], x[0]] for x in connections]
 
 #Part 1 + 2
-def find_path_Joker(path, scv, Joker):
+def find_path_Joker(path, Joker):
     if path[-1] == 'end':
         paths.append(path)
-        return True
+        return
 
     for x in [co for co in connections if co[0] == path[-1]]:
-        if (x[1] in scv and not Joker) or x[1] == 'start':
+        if not x[1].islower():  #Big cave
+            find_path_Joker(path+[x[1]], Joker)
+        elif (x[1] in path and not Joker) or x[1] == 'start':   #Dead end
             continue
-        elif x[1] in scv:   #Use Joker to visit sc twice
-            find_path_Joker(path+[x[1]], scv, False)
-        elif x[1] in smallcaves:
-            find_path_Joker(path+[x[1]], scv+[x[1]], Joker)
-        else: #big cave
-            find_path_Joker(path+[x[1]], scv, Joker)
-    return True
+        elif x[1] in path:   #Use Joker to visit sc twice
+            find_path_Joker(path+[x[1]], False)
+        else:   #new sc
+            find_path_Joker(path+[x[1]], Joker)
+            
+    return
 
 #Part 1
-paths, scv = [], ['start'] #small caves visited
-find_path_Joker(['start'],scv, False)
+paths = []
+find_path_Joker(['start'], False)
 print(len(paths))
     
 #Part 2
-paths, scv = [], ['start']
-find_path_Joker(['start'],scv, True)
+paths = []
+find_path_Joker(['start'], True)
 print(len(paths))
