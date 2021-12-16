d5d8b5abd8cc946fba12da79ee5ed10278368b5b
AvalasJT <79806487+AvalasJT@users.noreply.github.com>
2021-12-16 14:38:37 +0100

    Combined Part 1 and 2 in the same function

--- Day_16.py
+++ Day_16.py
@@ -2,6 +2,7 @@ with open('input.txt', 'r') as f:
     l = f.readline().strip()
     message = bin(int(l,16))[2:].zfill(len(l)*4) 
 
+#Part 1+ 2
 def get_literal_Value(m):
     last = False
     val = ''
@@ -14,38 +15,12 @@ def get_literal_Value(m):
     val = int(val,2)
     return val, m
 
-def get_sum_V(m):
+def interprete(m):
     if len(m) < 11:
         return 0, m
     V, m = int(m[:3],2), m[3:]
     T, m = int(m[:3],2), m[3:]
     V_sum = V
-    
-    if T == 4:
-        __, m = get_literal_Value(m)
-    else:
-        L_T_ID, m = int(m[0]), m[1:]
-        if L_T_ID == 0: #legnth of subpackage
-            len_sub_pack, m = int(m[:15],2), m[15:]
-            sub_m, m = m[:len_sub_pack], m[len_sub_pack:]
-            while sub_m != '':
-                sub_V, sub_m = get_sum_V(sub_m)
-                V_sum += sub_V
-        else: #number of subpackages
-            num_sub_pack, m = int(m[:11],2), m[11:]
-            for i in range(num_sub_pack):
-                sub_V, m = get_sum_V(m)
-                V_sum += sub_V
-    return V_sum, m
-
-V_sum, mes = get_sum_V(message)
-print(V_sum)
-
-#Part 2
-def interprete(m):
-    if len(m) < 11:
-        return 0, m
-    T, m = int(m[3:6],2), m[6:]
     
     if T == 4:
         Val, m = get_literal_Value(m)
@@ -56,12 +31,14 @@ def interprete(m):
             len_sub_pack, m = int(m[:15],2), m[15:]
             sub_m, m = m[:len_sub_pack], m[len_sub_pack:]
             while sub_m != '':
-                sub_Val, sub_m = interprete(sub_m)
+                sub_V, sub_Val, sub_m = interprete(sub_m)
+                V_sum += sub_V
                 sub_Vals.append(sub_Val)
         else:
             num_sub_pack, m = int(m[:11],2), m[11:]
             for i in range(num_sub_pack):
-                sub_Val, m = interprete(m)
+                sub_V, sub_Val, m = interprete(m)
+                V_sum += sub_V
                 sub_Vals.append(sub_Val)
                 
         if T == 0:
@@ -84,7 +61,8 @@ def interprete(m):
             Val = -999
             print('Invalid Type')
 
-    return Val, m
+    return V_sum, Val, m
         
-Val, mes = interprete(message)
+V_sum, Val, mes = interprete(m)
+print(V_sum)
 print(Val)
